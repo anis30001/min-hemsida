9e166234bf48fb5f7f7dadd29ee3fd9ce8530ed2
anis5 <anis5@hotmail.se>
2025-01-30 22:01:47 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const header=document.getElementById('header');
+header.style.color='blue';
+
+const highlights=document.querySelectorAll('.highlight')
+highlights.forEach(highlight => {;
+    highlight.style.color='purple';
+});
