9414ef71bbbb17f9b4682942040a382e0f1609a3
anis5 <anis5@hotmail.se>
2025-01-31 16:40:03 +0100

    responsiv last

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const header=document.getElementById('header');
-header.style.color='blue';
+const header = document.getElementById("header");
+header.style.color = "blue";
 
-const highlights=document.querySelectorAll('.highlight')
-highlights.forEach(highlight => {;
-    highlight.style.color='purple';
+const highlights = document.querySelectorAll(".highlight");
+highlights.forEach((highlight) => {
+  highlight.style.color = "black";
 });
